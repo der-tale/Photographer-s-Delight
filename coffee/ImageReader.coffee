757cc7c3eb4c610e4e4e52a0f482644e1c5dc46f
Manuel Zamora-Morschhaeuser <manuel@zamora.de>
2011-05-31 22:27:55 +0200

    Ported two methods from ImageReader

--- coffee/ImageReader.coffee
+++ coffee/ImageReader.coffee
@@ -1,13 +1,78 @@
 class ImageReader
 
     options: {}
+    images: []
+
+    imageMap: []
+
+    loadingList: []
+    loadingWaitList: []
 
     constructor: (options) ->
         options = if options then options else {}
         @options = _.defaults options, {
                     preloadCount: 5
                     parallelLoadCount: 1
+                    images: []
                 }
+
+        @startLoading()
+
+
+    startLoading: -> 
+
+        @imageMap = ({
+            finished: false
+            image: null
+            url: image
+        } for image in @options.images)
+         
+        @getImage(0)
+
+
+    getImage: (index, callback) ->
+        
+        # If we preload more than 2 images, preload from
+        # index - floor(preloadCount / 2) 
+        offset =    if @options.preloadCount > 2
+                        Math.floor @options.preloadCount / 2
+                    else
+                        0
+
+        # In doubt, preload more images coming up then previous ones
+        offset = if @options.preloadCount % 2 == 0 then offset - 1 else offset
+
+        begin = if (begin = index - offset) >= 0 
+                    begin 
+                else
+                    0
+
+        end =   if (end = index + @options.preloadCount - offset) < @imageMap.length 
+                    end
+                else
+                    @imageMap.length - 1
+        
+
+        current = begin 
+        @loadingWaitList = while (current <= end)
+            image = @imageMap[current]
+            if current == index 
+                image.callback = callback
+            else 
+                if @options.standardCallback? 
+                    image.callback = @options.standardCallback
+
+            current += 1
+
+            image
+
+
+
+    
+
+    
+
+    
 
    
 root = exports ? this
--- js/ImageReader.js
+++ js/ImageReader.js
@@ -2,13 +2,62 @@
   var ImageReader, root;
   ImageReader = (function() {
     ImageReader.prototype.options = {};
+    ImageReader.prototype.images = [];
+    ImageReader.prototype.imageMap = [];
+    ImageReader.prototype.loadingList = [];
+    ImageReader.prototype.loadingWaitList = [];
     function ImageReader(options) {
       options = options ? options : {};
       this.options = _.defaults(options, {
         preloadCount: 5,
-        parallelLoadCount: 1
+        parallelLoadCount: 1,
+        images: []
       });
+      this.startLoading();
     }
+    ImageReader.prototype.startLoading = function() {
+      var image;
+      this.imageMap = (function() {
+        var _i, _len, _ref, _results;
+        _ref = this.options.images;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          image = _ref[_i];
+          _results.push({
+            finished: false,
+            image: null,
+            url: image
+          });
+        }
+        return _results;
+      }).call(this);
+      return this.getImage(0);
+    };
+    ImageReader.prototype.getImage = function(index, callback) {
+      var begin, current, end, image, offset;
+      offset = this.options.preloadCount > 2 ? Math.floor(this.options.preloadCount / 2) : 0;
+      offset = this.options.preloadCount % 2 === 0 ? offset - 1 : offset;
+      begin = (begin = index - offset) >= 0 ? begin : 0;
+      end = (end = index + this.options.preloadCount - offset) < this.imageMap.length ? end : this.imageMap.length - 1;
+      current = begin;
+      return this.loadingWaitList = (function() {
+        var _results;
+        _results = [];
+        while (current <= end) {
+          image = this.imageMap[current];
+          if (current === index) {
+            image.callback = callback;
+          } else {
+            if (this.options.standardCallback != null) {
+              image.callback = this.options.standardCallback;
+            }
+          }
+          current += 1;
+          _results.push(image);
+        }
+        return _results;
+      }).call(this);
+    };
     return ImageReader;
   })();
   root = typeof exports !== "undefined" && exports !== null ? exports : this;
